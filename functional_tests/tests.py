a87cd73c891aab606840ce6db498c8938d62e609
mrhaffner <mrhaffner13@gmail.com>
2021-12-16 18:14:45 -0600

    Updated FT with start of user story

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -12,7 +12,13 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
         
 
-    def test_django_loads(self) -> None:
+    def test_can_view_top_news_words(self) -> None:
+        # Stevesie has heard about a new web page that tracks
+        # words used in the news.  He visits the homepage
         self.browser.get('http://localhost:8000')
 
-        self.assertIn('install worked', self.browser.title)
+        # He notices that that page title and header
+        # mention News Words
+        self.assertIn('Trending News Words', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1')
+        self.assertIn('Trending News Words', header_text)
