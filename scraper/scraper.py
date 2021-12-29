4d8802275b37e56012582010627cd34aaa217c02
mrhaffner <mrhaffner13@gmail.com>
2021-12-28 18:12:40 -0600

    Added test for scraper

---
+++ scraper/scraper.py
@@ -0,0 +1,5 @@
+#scraper gets list of websites to scrape from? Json? DB? YAML?
+
+#scraper iterates over those websites
+    #scraper gets a soup for each website
+    #scraper saves soup
--- scraper/tests/tests.py
+++ scraper/tests/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from bs4 import BeautifulSoup
+from scraper.scraper import Scraper
+from scraper.models import RSS_Post
 
 
 class ScraperTest(TestCase):
@@ -7,7 +9,14 @@ class ScraperTest(TestCase):
     def get_mock_page(self) -> None:
         with open('scraper/tests/mock_rss.html') as file:
             html = file.read()
+        return html
 
-        rss_feed = BeautifulSoup(html, 'html.parser')
-        return rss_feed
+        # rss_feed = BeautifulSoup(html, 'html.parser')
+        # return rss_feed
 
+    
+    def test_scraper_saves_rss_post(self) -> None:
+        mock_page = self.get_mock_page()
+        Scraper.save_rss_post(mock_page)
+        #add more logic for fields?
+        self.assertEqual(RSS_Post.objects.count(), 1)
